792d91407e9041e84a1a44d3def11469c39e6ab4
Temitayo Afolabi <temfoden@gmail.com>
2023-08-10 13:33:34 -0700

    update script to comply with betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int main(void)
 /**
  * main - Entry point
  *
@@ -7,7 +8,6 @@
  *              followed by a new line using the puts function.
  *              Returns: 0 (Success)
  */
-int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
