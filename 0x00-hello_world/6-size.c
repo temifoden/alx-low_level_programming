a2f660d23c964650492ef9e9ed1a9000ac902742
Temitayo Afolabi <temfoden@gmail.com>
2023-08-10 16:58:13 -0700

    script update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- *
  * Description: Prints the size of various types 
  * on the computer it is compiled and run on.
  * Returns: Always 0 (Success)
@@ -14,8 +13,5 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-/**
- * main -when return is 0 it is successful and fails if otherwise
- */
 return (0);
 }
