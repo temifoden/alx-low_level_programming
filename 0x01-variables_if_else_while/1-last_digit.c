1fe25553ac82008c300a2046e8bae52f65909241
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 14:48:05 -0700

    update script to comply with betty coding style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,16 +23,14 @@ lastDigit = n % 10;
 
 printf("Last digit of %d is %d", n, lastDigit);
 
-if (lastDigit > 5)
+if (lastDigit > 5) puts(" and is greater than 5");
+else if (lastDigit == 0)
 {
-puts(" and is greater than 5");
-}
-else if (lastDigit == 0){
 puts(" and is 0");
 }
 else
 {
 puts(" and is less than 6 and not 0");
 }
-return(0);
+return (0);
 }
