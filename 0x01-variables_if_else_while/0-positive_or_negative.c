76893a6601f33114ccaca2e8b7c5b08d27bc647b
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 13:58:47 -0700

    script update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,16 +17,18 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n==0)
+if (n == 0)
 {
 printf("%d is zero\n", n);
-}else if(n > 0)
+}
+else if (n > 0)
 {
 printf("%d is positive\n", n);
-} else
+}
+else
 {
 printf("%d is negative\n", n);
 }
 
-return(0);
+return (0);
 }
