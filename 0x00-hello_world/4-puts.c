75aebc2a4f40905c6f2f832c5e586bcf304e9858
Temitayo Afolabi <temfoden@gmail.com>
2023-08-10 17:13:47 -0700

    script updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: "Prints "Programming is like building a 
- * multilingual puzzle"
- *
- * Return: Alaways 0 (success)
- */
+* main - Entry point
+*
+* Description: "Prints "Programming is like building a 
+* multilingual puzzle"
+*
+* Return: Alaways 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
