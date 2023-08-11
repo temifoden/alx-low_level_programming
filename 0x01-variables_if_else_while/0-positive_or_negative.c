168e3901ee70a241ebec91866f917e1b9661947b
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 13:32:00 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "the program checks if the output of a
+ * random generated value is positive or negative"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+srand(time(0));
+
+int n = rand() - RAND_MAX / 2;
+printf("%d ", n);
+
+if (n == 0){
+printf("is zero\n");
+}else if(n > 0){
+printf("is positive\n");
+} else{
+printf("is negative\n");
+}
+return(0);
+}
