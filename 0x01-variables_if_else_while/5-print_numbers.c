1b1f6b246119b4e7e5687af7951ac90f1040b578
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 15:58:26 -0700

    script that print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "the program prints  0-9"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+num = 0;
+while (num < 10)
+{
+printf(num);
+num++;
+}
+printf("\n");
+return (0);
+}
