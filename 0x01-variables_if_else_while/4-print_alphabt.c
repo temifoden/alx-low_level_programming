4bbbcd0716ec024a6680c4c0f5f3b88e1cc8a5c9
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 15:51:40 -0700

    script that prints a-z omitting e&q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "the program prints lowercase a-z"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter;
+letter = 'a';
+while (letter <= 'z')
+{
+if(letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar(10);
+return (0);
+}
