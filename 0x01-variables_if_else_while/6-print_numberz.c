495dbe257a31cf9ca27a6666a4c1e2c1c316548a
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 17:44:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "the program prints  0-9"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+num = 0;
+while (num < 10)
+{
+putchar(num);
+num++;
+}
+putchar(10);
+return (0);
+}
