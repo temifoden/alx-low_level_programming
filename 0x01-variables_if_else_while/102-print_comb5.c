ff95425eddfbc062f5a9698d477359e55066406e
Temitayo Afolabi <temfoden@gmail.com>
2023-08-11 21:53:32 -0700

    script that prints all possible combination of 0-99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,20 +3,32 @@
 /**
  * main - Entry point
  *
- * Description: "the program prints lowercase z-a"
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-{
-char letter;
-letter = 'z';
-while (letter >= 'a')
-{
-putchar(letter);
-letter--;
-}
-putchar(10);
-return (0);
-}
+	{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 99)
+	{
+	j = i + 1;
+	while (j <= 99)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(' ');
+		putchar(j / 10 + '0');
+		putchar(j % 10 + '0');
+	if (i != 98 || j != 99)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		j++;
+	}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+	}
