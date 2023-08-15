ce56f706426c2f6369a9871565d85a622b5d612e
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 14:33:33 -0700

    betty coding style requirement fulfilled

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,11 @@
 #include "main.h"
 
 /**
+ * print_alphabet - Entry point
+ *
  * Description - function that prints a-z
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
