95281c3d233a9ceb47716623e4ac745bc8a3d09d
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 14:48:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description - function that prints a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int first_letter;
+int x;
+while(x < 10)
+{
+for (first_letter = 'a'; first_letter <= 'z'; first_letter++)
+{
+putchar(first_letter);
+}
+putchar('\n');
+x++
+}
+}
