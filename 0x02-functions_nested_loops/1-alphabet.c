aebdd820ff6619a9e04719e72c79c41f194f5228
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 14:19:13 -0700

    script update to comply with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_alphabet(){
+/**
+ * Description - function that prints a-z
+ * Return - success (0)
+ */
+
+void print_alphabet(void)
+{
 int first_letter;
 for (first_letter = 'a'; first_letter <= 'z'; first_letter++)
 {
 putchar(first_letter);
 }
+putchar(10);
 }
