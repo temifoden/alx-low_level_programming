6f99e93302749ebb96a258bc4983885b320b3d94
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 12:22:28 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0.
+ */
+
 int main (void)
 {
 printf("_putchar\n");
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - write function that print a-z
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(){
+int first_letter;
+for (first_letter = 'a'; first_letter <= 'z'; first_letter++)
+{
+putchar(first_letter);
+}
+}
+
+int main ()
+{
+print_alphabet();
+putchar(10);
+return (0);
+}
