1a6c6be767ec09570ea8b2f6dfcd39333ec4890b
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 21:29:46 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - entry
+ *
+ * Description - function that prints a-z
+ *
+ * @x: int x is the integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
