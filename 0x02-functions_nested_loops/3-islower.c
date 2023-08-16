f140f3edce414cfb4269a316f9a3ffb4f7fa3ae6
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 18:38:20 -0700

    script update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,16 +6,18 @@
  *
  * Description - function that prints a-z
  *
+ * @c: int c is the integer
+ *
  * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-    return 1;
-      }else
-    {
-    return 0;
-  }
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}else
+{
+return (0);
+}
 }
