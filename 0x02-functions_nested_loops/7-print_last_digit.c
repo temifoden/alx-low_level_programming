fda41327e4dce17046094749fd6a60abbb437d96
Temitayo Afolabi <temfoden@gmail.com>
2023-08-16 09:48:03 -0700

    script update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,5 +15,6 @@ int print_last_digit(int n)
 int last_digit = n % 10;
 int positive_val;
 positive_val = (last_digit < 0) ? -last_digit : last_digit;
+_putchar('0' + positive_val);
 return (positive_val);
 }
