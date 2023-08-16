e1793b0c4f8fcc403d797fb76be7239a6c5dd6e3
Temitayo Afolabi <temfoden@gmail.com>
2023-08-16 13:58:36 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - entry
+ *
+ * Description - function that prints a-z
+ *
+ * @x: int x is the first
+ * @y: int y is the second parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+int result = x + y;
+return (result);
+}
