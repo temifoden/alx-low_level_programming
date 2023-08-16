d1fed39ca2e38d86d53ee2168d4d8bf99f288295
Temitayo Afolabi <temfoden@gmail.com>
2023-08-15 21:58:25 -0700

    all

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry
+ *
+ * Description - function that prints a-z
+ *
+ * @n: int x is the integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+return (last_digit);
+}
