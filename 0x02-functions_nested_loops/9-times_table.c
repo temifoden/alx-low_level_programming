eb879ac525f7412ca3ec95e21438ebf02d5afcb2
Temitayo Afolabi <temfoden@gmail.com>
2023-08-16 12:30:18 -0700

    script updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@
 void times_table(void)
 {
 int x, y;
-for (x = 0; x <= 9; x++)
+for (x = 0; x < 9; x++)
 {
-for (y = 0; y <= 9; y++)
+for (y = 0; y < 9; y++)
 {
 int out;
 out = x * y;
@@ -27,10 +27,10 @@ else
 {
 _putchar('0' + out);
 }
-if (y != 9)
+if (y != 8)
 {
 _putchar(',');
-_putchar('\t');
+_putchar(' ');
 }
 }
 _putchar(10);
