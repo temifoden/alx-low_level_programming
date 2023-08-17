65b68133a67734f33d91d77b21d6c31748045c98
Temitayo Afolabi <temfoden@gmail.com>
2023-08-17 15:15:51 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - entry
+ *
+ * Description - function that prints a-z
+ *
+ * @a: is the first parameter
+ * @b: is the second parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+int result = x * y;
+return (result);
+}
