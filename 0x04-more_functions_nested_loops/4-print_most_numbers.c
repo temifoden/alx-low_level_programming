7ed918c6261145af8d7b377ea5846deab5c60300
Temitayo Afolabi <temfoden@gmail.com>
2023-08-17 15:38:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry
+ *
+ * Description - print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{    
+_putchar('0' + i);
+}
+}
+_putchar(10);
+}
