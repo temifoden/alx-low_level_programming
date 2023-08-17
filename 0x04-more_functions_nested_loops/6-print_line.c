cde62bde501413afdc1adc3940e3cbfe3ed962d7
Temitayo Afolabi <temfoden@gmail.com>
2023-08-17 16:45:50 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ *
+ * Description - function prints 0-14, 10times
+ * @n: 1 and only param
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+{
+putchar(10);
+}
+else
+{
+int i;
+for (i = 0; i < n; i++)
+{
+putchar('_');
+}
+putchar(10);
+}
+}
