e546ccbb68bc5b7a8538c711edf5e508e93c59f5
Temitayo Afolabi <temfoden@gmail.com>
2023-08-17 12:10:58 -0700

    file updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 
+int main(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-convert_day(int month, int day);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif
