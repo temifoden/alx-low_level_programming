17cc00d6fa2202318dd249f069d5e6ec8b6c136a
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-21 23:13:55 +0100

    source code that generate random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+
+/**
+ * main - Generates a random valid password for the program 101-crackme.
+ *
+ * This program generates a random password that consists of valid characters
+ * for the program 101-crackme. It uses the rand() function to generate random
+ * numbers and seeds the random number generator using the current time. The
+ * generated password is printed to the console.
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+srand(time(NULL));
+
+
+char validChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int validCharsCount = sizeof(validChars) - 1;
+
+char password[PASSWORD_LENGTH + 1];
+
+printf("Generating valid password for 101-crackme...\n");
+
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+int randomIndex = rand() % validCharsCount;
+password[i] = validChars[randomIndex];
+}
+
+password[PASSWORD_LENGTH] = '\0';
+
+printf("Generated password: %s\n", password);
+
+return 0;
+}
