c0f1d5d72669c3f2c59c696e4c2f7cbb6c1e41bc
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-21 21:07:45 +0100

    add source code that prints character of a pointer to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: pointer to the input string.
+ *
+ * Description - This function prints the characters of the
+ * string to the standard output, followed by a new line
+ *
+ * Return: None.
+ */
+
+int _puts(char *str)
+{
+puts(str);
+}
