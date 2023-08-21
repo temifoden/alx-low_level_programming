cf44bf3caa45641a94d93b17d2879dd0975c0bea
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-21 21:41:47 +0100

    add source code that print reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a reversed string followed by a new line to stdout.
+ * @s: pointer to the input string.
+ *
+ * Description - This function prints the reversed characters of the
+ * string to the standard output, followed by a new line
+ *
+ * Return: None.
+ */
+
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+while (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar(10);
+}
