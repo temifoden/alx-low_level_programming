a880b2842e18518f5dda087661aa1e51cdccb96d
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-21 23:30:22 +0100

    add source code 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints a string followed by a new line to stdout.
+ * @str: pointer to the input string.
+ *
+ * Description - This function prints the characters of the
+ * string to the standard output, followed by a new line
+ *
+ * Return: None.
+ */
+
+void puts2(char *str)
+{
+int i;
+i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar(10);
+}
