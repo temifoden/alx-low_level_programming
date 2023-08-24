d77415da46858a9d7f33b21af6f18b6a778aba18
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-24 17:55:01 +0100

    update source code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
+while (*s1 && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
 
-    return (*s1 - *s2);
+return (*s1 - *s2);
 }
