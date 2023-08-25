e7309792adbba215abf903af36b6b87e2274e763
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-25 12:26:36 +0100

    update source code to comply with betty coding style

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,7 +9,8 @@
 
 void print_number(int n)
 {
-if (n < 0){
+if (n < 0)
+{
 _putchar('-');
 n = -n;
 }
@@ -20,7 +21,7 @@ _putchar('0' + n);
 }
 else if (n >= 10 && n < 100)
 {
-_putchar('0'+ n / 10);
+_putchar('0' + n / 10);
 _putchar('0' + n % 10);
 }
 else if (n >= 100 && n < 999)
