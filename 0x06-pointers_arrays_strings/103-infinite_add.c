de0415ff9941eac4545008a3528af66925d217d9
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-27 20:27:11 +0100

    update source code

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,55 +11,55 @@
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0;
-    int len2 = 0;
-    int carry = 0;
-    int max_length = size_r - 1;
+int len1 = 0;
+int len2 = 0;
+int carry = 0;
+int max_length = size_r - 1;
 
-    while (n1[len1] != '\0')
-    {
-        len1++;
-    }
-    while (n2[len2] != '\0')
-    {
-        len2++;
-    }
+while (n1[len1] != '\0')
+{
+len1++;
+}
+while (n2[len2] != '\0')
+{
+len2++;
+}
 
-    if (len1 > max_length || len2 > max_length)
-    {
-        return 0;
-    }
+if (len1 > max_length || len2 > max_length)
+{
+return (0);
+}
 
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int k = 0;
+int i = len1 - 1;
+int j = len2 - 1;
+int k = 0;
 
-    while (i >= 0 || j >= 0 || carry > 0) {
-        int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-        int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+while (i >= 0 || j >= 0 || carry > 0) {
+int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+int digit2 = (j >= 0) ? n2[j] - '0' : 0;
 
-        int sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        r[k] = (sum % 10) + '0';
-        k++;
+int sum = digit1 + digit2 + carry;
+carry = sum / 10;
+r[k] = (sum % 10) + '0';
+k++;
 
-        i--;
-        j--;
-    }
+i--;
+j--;
+}
 
-    r[k] = '\0';
+r[k] = '\0';
 
     
-    int start = 0;
-    int end = k - 1;
-    while (start < end)
-    {
-        char temp = r[start];
-        r[start] = r[end];
-        r[end] = temp;
-        start++;
-        end--;
-    }
+int start = 0;
+int end = k - 1;
+while (start < end)
+{
+char temp = r[start];
+r[start] = r[end];
+r[end] = temp;
+start++;
+end--;
+}
 
-    return r;
+return (r);
 }
