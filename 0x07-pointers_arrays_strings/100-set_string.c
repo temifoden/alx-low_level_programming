c4cabfd8ba101fe8511c69fa4419162e6c70de52
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-29 00:34:19 +0100

    new source code

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - prints an integer.
+ * @s: The input value.
+ * @to: the character
+ *
+ * Return: Nothing
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
