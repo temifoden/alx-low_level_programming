d67d6aa25f501df51acaf7218c4cc0babec38707
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-29 17:22:53 +0100

    update source code to comply with betty coding style

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,22 +16,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    while (*haystack != '\0')
-    {
-        char *h = haystack;
-        char *n = needle;
+while (*haystack != '\0')
+{
+char *h = haystack;
+char *n = needle;
 
-        while (*n != '\0' && *h == *n)
-        {
-            h++;
-            n++;
-        }
+while (*n != '\0' && *h == *n)
+{
+h++;
+n++;
+}
 
-        if (*n == '\0')
-        {
-            return (haystack);
-        }
-        haystack++;
-    }
-    return (NULL);
+if (*n == '\0')
+{
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
 }
