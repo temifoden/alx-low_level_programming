e957debd448a64ccc56fb327d3e2e1470af7b737
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-29 11:50:02 +0100

    update source code to comply with betty coding style

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,22 @@
 #include <stddef.h>
 
 
-
 /**
  * _strchr - Entry; starting point
  * @s: string to check
  * @c: character to check in the s string
- * 
+ *
  * Description - the function checks for an occurence of a
  * character in a set of string and return from first occurence
- * 
+ *
  * Return: return the pointer to first occurence
  * or NULL if not found
 */
 
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; *s != '\0'; i++)
+
+while (*s != '\0')
 {
 if (*s == c)
 {
