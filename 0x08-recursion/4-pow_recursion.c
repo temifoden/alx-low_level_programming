596c1a35e4d508dc5ada14b8079b36ae89ad39e2
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-30 22:15:14 +0100

    add source code that recursively calculate power of numbers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates x raised to the power of y recursively.
+ *
+ * @x: The base.
+ * @y: The exponent.
+ *
+ * Return: The value of x raised to the power of y, or -1 if y is negative.
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+
+if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
