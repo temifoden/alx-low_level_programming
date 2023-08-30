1502938ce2d99ce2fdca33171339e0d540b80bcd
Temi Afolabi <linuxmathew1245@gmail.com>
2023-08-31 00:41:38 +0100

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * wildcmp - Compares two strings with wildcard support.
+ *
+ * @s1: The first input string.
+ * @s2: The second input string with wildcard support.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+{
+return (1);
+}
+
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '\0')
+{
+return (1);
+}
+
+while (*s1 != '\0')
+{
+if (wildcmp(s1, s2 + 1))
+{
+return (1);
+}
+s1++;
+}
+
+return (wildcmp(s1, s2 + 1));
+}    
+if (*s1 == *s2) {
+return (wildcmp(s1 + 1, s2 + 1));
+}
+    
+return (0);
+}
