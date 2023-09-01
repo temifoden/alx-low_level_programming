d887623e9f0b897cba2e4889dea1fdd9d455e3a4
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-01 18:56:05 +0100

    add directory arguments tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description:
+ *   This program prints its name, including the path, without having to
+ *   recompile it when you change its name. It uses the command-line arguments
+ *   provided to determine its name.
+ *
+ * Return:
+ *   Always returns 0 to indicate successful execution.
+ */
+
+int main(int argc, char *argv[]){
+    if (argc > 0){
+        printf("%s\n", argv[0]);
+    }
+    return (0);
+}
