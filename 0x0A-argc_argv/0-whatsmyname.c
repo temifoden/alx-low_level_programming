bffc82ed7e9fc14492fafeef9112b958927b4eae
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-01 19:00:06 +0100

    add readme file to directory

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,9 +12,11 @@
  *   Always returns 0 to indicate successful execution.
  */
 
-int main(int argc, char *argv[]){
-    if (argc > 0){
-        printf("%s\n", argv[0]);
-    }
-    return (0);
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
 }
