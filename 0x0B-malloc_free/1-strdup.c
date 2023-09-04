70e6cf411ce97c344c76323357a4e9bc5c7c58d3
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-04 19:12:14 +0100

    add strdup.c source file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/**
+ * _strdup - Duplicates a string in memory.
+ * @str: The string to be duplicated.
+ *
+ * Return: A pointer to the newly allocated duplicated string, or NULL if str is NULL
+ *         or if insufficient memory was available.
+ */
+
+char *_strdup(char *str)
+{
+char *duplicate;
+unsigned int length, i;
+length = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[length])
+{
+length++;
+}
+duplicate = (char *)malloc((length + 1) * sizeof(char));
+
+if (duplicate == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < length; i++)
+{
+duplicate[i] = str[i];
+}
+duplicate[length] = '\0';
+
+return (duplicate);
+}
