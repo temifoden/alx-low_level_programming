770238d03aab86b61a5ca05c274cbccc10e79c64
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-04 18:09:31 +0100

    update script to comply with betty coding style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
 
+
+/**
+ * create_array - Creates an array of characters and
+ * initializes it with a specific character.
+ *
+ * @size: The size of the array to create.
+ * @c: The character used for initialization.
+ *
+ * Return: A pointer to the allocated array, or NULL
+ * if size is 0 or if memory allocation fails.
+ */
 
 char *create_array(unsigned int size, char c)
 {
