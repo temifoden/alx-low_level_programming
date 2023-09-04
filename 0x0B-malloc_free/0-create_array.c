eadbbc68815f176d9da34743c78ef466845f9c44
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-04 18:15:30 +0100

    update source code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,13 @@
 char *create_array(unsigned int size, char c)
 {
 int unsigned i;
+char *arr;
 if (size == 0)
 {
 return NULL;
 }
 
-char *arr = (char *)malloc(sizeof(char) * size);
+arr = (char *)malloc(sizeof(char) * size);
 if(arr == NULL)
 {
 return NULL;
