de3352a699f37a1d09f2515d7bd81ac826b12812
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-05 16:10:37 +0100

    update source code to comply with betty coding style

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,10 @@ int **alloc_grid(int width, int height)
 int **grid;
 int i, j;
 
-if (width <= 0 || height <= 0) return (NULL);
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
 grid = (int **)malloc(height * sizeof(int *));
 
@@ -40,8 +43,10 @@ for (i = 0; i < height; i++)
 grid[i] = (int *)malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
-for(j = 0;j < i; j++) 
+for (j = 0; j < i; j++)
+{
 free(grid[j]);
+}
 
 free(grid);
 return (NULL);
