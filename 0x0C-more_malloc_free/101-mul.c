6a3776dc92fc527d515156a6c5483f82cbda405a
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-11 12:38:46 +0100

    update source code to handle large numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * isPositiveInteger - code entry
+ * @str: parameter to check
+ * 
+ * Return: 1 on success and 0 if failed
+*/
+
+int isPositiveInteger(char *str){
+    int i;
+    if (str == NULL || str[0] == '\0')
+        return (0);
+    
+    for (i = 0; str[i] != '\0'; i++){
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+    }
+
+    return (1);
+}
+
 /**
  * main - Entry point of the program.
  *
@@ -18,9 +39,11 @@
 
 int main(int argc, char *argv[])
 {
-int num1;
-int num2;
-int result;
+unsigned long num1;
+unsigned long num2;
+unsigned long result;
+char *num1_str;
+char *num2_str;
 
 if (argc != 3)
 {
@@ -28,12 +51,20 @@ printf("Error\n");
 exit(98);
 }
 
+num1_str = (argv[1]);
+num2_str = (argv[2]);
 
+if (!isPositiveInteger(num1_str) || !isPositiveInteger(num2_str))
+{
+    printf("Error\n");
+    exit(98);
+}
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
+num1 = strtoul(num1_str, NULL, 10);
+num2 = strtoul(num2_str, NULL, 10);
 
-printf("%d\n", result);
+result = (num1 * num2);
+
+printf("%lu\n", result);
 return (0);
 }
