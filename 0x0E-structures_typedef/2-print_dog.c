ec474265cdf7755024c0b952d130e12adc22f935
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-12 21:45:00 +0100

    update source code to comply with betty coding style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,34 +10,34 @@
 
 void print_dog(struct dog *d)
 {
-    if (d != NULL)
-    {
+if (d != NULL)
+{
 
-        if (d->name == NULL)
-        {
-            printf("Name: nil\n");
-        }
-        else
-        {
-            printf("Name: %s\n", d->name);
-        }
+if (d->name == NULL)
+{
+printf("Name: nil\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
 
-        if (d->age >= 0)
-        {
-            printf("Age: nil\n");
-        }
-        else
-        {
-            printf("Age: %.1f\n", d->age);
-        }
+if (d->age <= 0)
+{
+printf("Age: nil\n");
+}
+else
+{
+printf("Age: %.1f\n", d->age);
+}
 
-        if (d->owner == NULL)
-        {
-            printf("Owner: nil\n");
-        }
-        else
-        {
-            printf("Owner: %s\n", d->owner);
-        }
-    }
+if (d->owner == NULL)
+{
+printf("Owner: nil\n");
+}
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
 }
