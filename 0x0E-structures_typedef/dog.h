cbc7a827594fa6bb443e82cdc620c7ecfc7184e7
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-12 20:21:16 +0100

    add directory that contain structures and typedef tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+ * struct dog - the struct name
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
