a697492bdcb6564e3f936238e74ecb436f0425fe
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-14 10:39:42 +0100

    update source code to comply with betty coding style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,13 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name- function entry
  * @name: first parameter
  * @f: pointer to a function
+ *
+ * Description - function that calls a function through it function
+ * pointer and pass an argument
  *
  * Return: Nothing
 */
