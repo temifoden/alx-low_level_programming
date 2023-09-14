0d6647945d1b587a35f1fa047f153d009e7901a5
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-14 11:07:18 +0100

    add source code that returns index if an array as determined by another function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,27 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
- * int_index
+ * int_index - function entry
+ * @array: first parameter
+ * @size: no of elements in the array
+ * @cmp: function pointer parameter
+ *
+ * Return: index on success. -1 on failure
 */
 
-int int_index(int *array, int size, int (*cmp)(int)){
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
 
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+
+return (-1);
 }
