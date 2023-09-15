4743445d22a7315470f9f4d176adb9c2113b61cb
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-15 06:15:18 +0100

    add conditions to handle some error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,40 +13,31 @@
 void print_all(const char * const format, ...){
     int i;
     char *str;
-    int printed;
     va_list my_list;
     va_start(my_list, format);
     i = 0;
-    printed = 0;
-
-    while(format[i]){
-        
-        if (printed)
-            printf(", ");
-
-        switch (format[i])
+    while(format[i])
+    {    
+    switch (format[i])
         {
         case 'c':
             printf("%c", va_arg(my_list, int));
-            printed = 1;
             break;
         case 'i':
             printf("%d", va_arg(my_list, int));
-            printed = 1;
             break;
         case 'f':
             printf("%f", va_arg(my_list, double));
-            printed = 1;
             break;
         case 's':
             str = va_arg(my_list, char *);
             str == NULL ? printf("(nil)") : printf("%s", str);
-            printed = 1;
             break;
         default:
             break;
         }
-
+        if (format[i + 1] && (format[i] == 'i' || format[i] == 'c' || format[i] == 's' || format[i] == 'f'))
+        printf(", ");
         i++;
     }
     va_end(my_list);
