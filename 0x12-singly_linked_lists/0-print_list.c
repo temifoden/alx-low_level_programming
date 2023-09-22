7056c591fb1b1904b59f86cd61aafa17083153e8
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-22 16:08:33 +0100

    add functionality that add new line after printing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,10 +18,10 @@ size_t print_list(const list_t *h)
     while(h != NULL)
     {
         if (h->str == NULL)
-            printf("[0] (nil)");
+            printf("[0] (nil)\n");
         
         else
-            printf("[%d] %s", h->len, h->str);
+            printf("[%d] %s\n", h->len, h->str);
 
         h = h->next;
         count++;
