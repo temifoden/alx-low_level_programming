1606f2c4eb5fb85598b8d6d3118ca41bd188795a
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-27 05:15:06 +0100

    add 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - Function frees the memory space of a linked list
+ * @head: pointer to the head pointer
+ *
+ * Return: Return count
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+size_t count;
+listint_t *current = *h;
+
+while (current != NULL)
+{
+listint_t *next_node = current->next;
+free(current);
+current = next_node;
+count++;
+}
+*h = NULL;
+return (count);
+}
