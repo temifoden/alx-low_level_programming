e71fbb71b33f8307086b6c21147779071bd80c95
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-27 03:40:50 +0100

    update source file to comply with betty coding style

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,18 +13,17 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL;
-    listint_t *current = *head;
+listint_t *prev = NULL;
+listint_t *current = *head;
 
-    while (current != NULL)
-    {
-        listint_t *next_node = current->next;
-        current->next = prev;
+while (current != NULL)
+{
+listint_t *next_node = current->next;
+current->next = prev;
 
-        prev = current;
-        current = next_node;
-
-    }
-    *head = prev;
-    return (prev);
+prev = current;
+current = next_node;
+}
+*head = prev;
+return (prev);
 }
