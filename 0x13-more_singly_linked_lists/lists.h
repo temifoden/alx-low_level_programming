9d43bb035cdcb9f584ab013b1d00115987959251
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-27 05:39:59 +0100

    add 103-find_loop.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ void free_listint2(listint_t **head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
