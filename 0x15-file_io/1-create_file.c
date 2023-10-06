33afb696c2d1280a32f1d8fa8f79623a201f7ed9
Temi Temi <linuxmathew1245@gmail.com>
2023-10-06 10:28:56 +0100

    add ./1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file -create a file with specific permissions and write content to it
+ *
+ * @filename: the name of the file to create
+ * @text_content: A NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ * -1 if the file cannot be created, written to, or other errors occur.
+ * 1 if the file is successfully created and written to.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+    if (filename == NULL)
+    return (-1);
+
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+    if (fd == -1)
+    return (-1);
+
+    if (text_content != NULL)
+    {
+        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+    close(fd);
+    return (1);
+}
