f234ae8b1e18287c06c8c41d7dd95b2626ad9cad
Temi Temi <linuxmathew1245@gmail.com>
2023-10-06 10:34:29 +0100

    update ./1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
+    int fd;
+    ssize_t bytes_written;
     if (filename == NULL)
     return (-1);
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
     if (fd == -1)
     return (-1);
 
     if (text_content != NULL)
     {
-        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+        bytes_written = write(fd, text_content, strlen(text_content));
 
         if (bytes_written == -1)
         {
